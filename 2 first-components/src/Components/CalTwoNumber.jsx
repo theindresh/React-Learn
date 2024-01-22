fb06a53774005047cc4d0774a347c944590d7742
INDRESH BHARATI <85506515+theindresh@users.noreply.github.com>
2024-01-22 23:36:36 +0530

    done

---
+++ 2 first-components/src/Components/CalTwoNumber.jsx
@@ -0,0 +1,7 @@
+function CalTwoNumber() {
+  let a, b;
+  a = 10;
+  b = 20;
+  return <h1>{`the out is: ${a + b}`}</h1>;
+}
+export default CalTwoNumber;
--- 2 first-components/src/Components/HelloWorld.jsx
+++ 2 first-components/src/Components/HelloWorld.jsx
@@ -1,10 +1,12 @@
 import ButtonForNewPage from "./ButtonForNewPage";
+import CalTwoNumber from "./CalTwoNumber";
 function HelloWorld() {
   return (
     <>
       <h1>Hello Indresh Bharati how are you</h1>
       <div>
         <ButtonForNewPage />
+        <CalTwoNumber />
       </div>
     </>
   );
