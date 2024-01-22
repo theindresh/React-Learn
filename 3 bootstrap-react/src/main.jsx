8d6fcfa45792da637c9e6f8c0598c3596d253843
INDRESH BHARATI <85506515+theindresh@users.noreply.github.com>
2024-01-22 23:46:46 +0530

    done

--- 3 bootstrap-react/src/main.jsx
+++ 3 bootstrap-react/src/main.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "../node_modules//bootstrap/dist/css/bootstrap.min.css";
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
